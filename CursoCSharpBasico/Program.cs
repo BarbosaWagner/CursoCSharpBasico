abe0a3d6672e9294f94f1107ebd99008eae6c7ba
Wagner Barbosa <wagner.barbosa@retornar.com.br>
2024-02-25 20:55:29 -0300

    Add project files.

---
+++ CursoCSharpBasico/Program.cs
@@ -0,0 +1,11 @@
+﻿// Aula 3: Variáveis
+
+// Tipos primitivos: https://www.alura.com.br/apostila-csharp-orientacao-objetos/variaveis-e-tipos-primitivos
+
+// Principais tipos de dados.
+var indicaClienteAtivo = true;
+var idade = 28;
+var valorTotal = 10.5;
+var letra = 'a';
+var nome = "Lucas Fábio";
+DateTime? data = DateTime.Now;
